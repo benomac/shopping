818630bd85e6f560170712efa1b78becfe2920b7
benomac <64007619+benomac@users.noreply.github.com>
2021-03-22 19:00:40 +0000

    Changed map to contain class instances instead of Arrays

--- shopping/src/main/scala/Main.scala
+++ shopping/src/main/scala/Main.scala
@@ -1,44 +1,45 @@
 object Main extends App {
     
   //make case class
-  val prices = Map('a' -> Array(50, 3, 130), 'b' -> Array(30, 2, 45), 'c' -> Array(20, 0, 0), 'd' -> Array(15, 0, 0))
+  case class Price (price: Int, num_for_deal: Int, price_4_deal: Int)
 
-  
+  //Map of classes
+  var prices = Map('a' -> Price(50, 3, 130), 'b' -> Price(30, 2, 45), 'c' -> Price(20, 0, 0), 'd' -> Price(15, 0, 0))
 
   //Create var for items to buy
   var items = scala.io.StdIn.readLine("Enter items list eg: abcd :-")
   
-  
-  var tobuy:Map[Char,Int] = Map()
-  
-  for(x <- items ){
-    var i = items.count(_ == x)
-    tobuy += (x -> i)
-  } 
-  
+  // CHANGE THIS TO BE INITIALIZED DURING DECLARATION
+
+  val tobuy =
+  (for {
+    x <- items
+    i = items.count(_ == x)
+  } yield (x -> i)).toMap
+
   var total: Int = 0
   
   def spesh (count: Int, normal_price: Int,  am_for_deal: Int, price_for_deal: Int) : Int = {
       
       val num_of_deals = count / am_for_deal
 
-      var no_deals = count % am_for_deal
+      val no_deals = count % am_for_deal
 
-      var price_for_no_deals = no_deals * normal_price
+      val price_for_no_deals = no_deals * normal_price
 
-      var price_for_deals = num_of_deals * price_for_deal
+      val price_for_deals = num_of_deals * price_for_deal
 
-      var tot = price_for_deals + price_for_no_deals
+      val tot = price_for_deals + price_for_no_deals
 
       tot
 
   }
 
   for ((key,value) <- tobuy) {
-      if(prices(key)(1) == 0) {
-        total = total + (value * prices(key)(0))
+      if(prices(key).num_for_deal == 0) {
+        total = total + (value * prices(key).price)
       } else {
-        var tot = spesh(value, prices(key)(0), prices(key)(1), prices(key)(2))
+        var tot = spesh(value, prices(key).price, prices(key).num_for_deal, prices(key).price_4_deal)
       total = total + tot
       }
     }
@@ -46,11 +47,4 @@ object Main extends App {
 
   println("total " + total)
         
-        
-            
-
-    
-
-    
-    
 }
