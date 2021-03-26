525ad2fd8cfb55f73529d6e1933adcb0e24c8344
benomac <64007619+benomac@users.noreply.github.com>
2021-03-26 12:03:20 +0000

    displayMessageForInvalidItems() created, not right though

--- shopping/src/main/scala/Main.scala
+++ shopping/src/main/scala/Main.scala
@@ -11,23 +11,32 @@
 
     
     //Get the price for any deals made
-    def priceForDeals (count: Int, things: Price): Int = {
-      (count / things.numForDeal) * things.priceForDeal
+    def priceForDeals (count: Int, products: Price): Int = {
+      (count / products.numForDeal) * products.priceForDeal
     }
 
     //Get the price for item that didnt make deal.
-    def priceForNoDeals (count: Int, things: Price): Int = {
-      (count % things.numForDeal) * things.price
+    def priceForNoDeals (count: Int, products: Price): Int = {
+      (count % products.numForDeal) * products.price
     }
 
     //get the price for item that have no deal
-    def priceWhenItemHasNoDealValues (count: Int, things: Price): Int = {
-      count * things.price
+    def priceWhenItemHasNoDealValues (count: Int, products: Price): Int = {
+      count * products.price
     }
 
     //Check item is in the prices map
-    def checkItemIsValid (item: Char): Boolean = {
-        prices.keySet.contains(item)
+    def checkItemIsValid (product: Char, products: Map[Char, Price]): Boolean = {
+        products.keySet.contains(product)
+    }
+
+    //Display message if an invalid item is found
+    def displayMessageForInvalidItems (products: String): Unit = {
+      for (item <- products) {
+      if (!(checkItemIsValid(item, prices))) {
+        println(item + " is an invalid item.")
+        }
+      }   
     }
     
   
@@ -35,21 +44,21 @@
     val items = scala.io.StdIn.readLine("Enter items list eg: abcd :-")
     
     // Map of things to buy, check and discards any invalid items.
-    val tobuy =
+   
+   // PUT EVERYTHING BELOW INTO FUNCTIONS 
+   // THEN PUT INTO THEIR OWN FILE
+   
+   val tobuy =
     (for {
       x <- items
       i = items.count(_ == x) 
       //function to check if item is valid.
-      if (checkItemIsValid(x))
+      if (checkItemIsValid(x, prices))
     } yield (x -> i)).toMap
 
-    //Display message if an invalid item is found
-    for (item <- items) {
-      if (!(checkItemIsValid(item))) {
-        println(item + " is an invalid item.")
-      }
-    }
     
+    displayMessageForInvalidItems(items)
+
     //Loop over tobuy map using item price data from prices
     println("Total " + {
     for ((key,value) <- tobuy) yield {
--- shopping/src/test/scala/ShoppingSpec.scala
+++ shopping/src/test/scala/ShoppingSpec.scala
@@ -6,27 +6,27 @@ class ShoppingSpec extends AnyFlatSpec with Matchers {
     
     // case class Price (price: Int, numForDeal: Int, priceForDeal: Int)
     
-    val prices = Map('a' -> Price(50, 3, 130), 
+    val costs = Map('a' -> Price(50, 3, 130), 
                      'b' -> Price(30, 2, 45), 
                      'c' -> Price(20, 0, 0), 
                      'd' -> Price(15, 0, 0))
 
-    behavior of "prices"   
+    behavior of "costs"   
 
     it should "get the price for 'a'" in {
-        prices('a').price shouldBe 50
+        costs('a').price shouldBe 50
     }           
 
     it should "get the numForDeal for 'a' " in {
-        prices('a').numForDeal shouldBe 3
+        costs('a').numForDeal shouldBe 3
     }
 
     it should "get the priceForDeal for 'a' " in {
-        prices('a').priceForDeal shouldBe 130
+        costs('a').priceForDeal shouldBe 130
     }
 
     it should "not get the price for 'a'" in {
-        prices('b').priceForDeal should not be 130
+        costs('b').priceForDeal should not be 130
     }
     
     
@@ -34,49 +34,42 @@ class ShoppingSpec extends AnyFlatSpec with Matchers {
     behavior of "priceForDeals"
 
     it should "return the cost for the amount of deals" in {
-        priceForDeals(3, prices('a')) shouldBe 130
-        priceForDeals(4, prices('a')) shouldBe 130
-        priceForDeals(10, prices('a')) shouldBe 390
+        priceForDeals(3, costs('a')) shouldBe 130
     }
 
     it should "not return the price for the deal" in {
-        priceForDeals(1, prices('a')) should not be 130
+        priceForDeals(1, costs('a')) should not be 130
     }
 
 
     behavior of "priceForNoDeals" 
 
     it should "return the price for left over item that didn't make the deal" in {
-        priceForNoDeals(4, prices('a')) shouldBe 50
+        priceForNoDeals(4, costs('a')) shouldBe 50
     }
 
     it should "not return the price for left over item that didn't make the deal" in {
-        priceForNoDeals(4, prices('a')) should not be 130
+        priceForNoDeals(4, costs('a')) should not be 130
     }
 
 
     behavior of "priceWhenItemHasNoDealValues" 
 
     it should "return the price for a number of items that have no deal values" in {
-        priceWhenItemHasNoDealValues(3, prices('c')) shouldBe 60
+        priceWhenItemHasNoDealValues(3, costs('c')) shouldBe 60
     }
 
     
 
 
-    // MAYA Why do i need to include this function in my tests to get the below to pass?
-    def checkItemIsValid (item: Char): Boolean = {
-        prices.keySet.contains(item)
-    }
-
     behavior of "checkItemIsValid"
 
     it should "return true" in {
-        checkItemIsValid('a') shouldBe true
+        checkItemIsValid('a', costs) shouldBe true
     }
 
     it should "return false" in {
-        checkItemIsValid('x') shouldBe false
+        checkItemIsValid('x', costs) shouldBe false
     }
 
 }
